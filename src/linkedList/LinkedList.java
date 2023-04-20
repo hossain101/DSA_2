989ba5a3a67eb006bbeacafa5d32c6c255a8e18c
shoaib <shoaibhossain24@gmail.com>
2023-04-21 00:06:58 +0600

    Add insert method

--- src/linkedList/LinkedList.java
+++ src/linkedList/LinkedList.java
@@ -1,5 +1,7 @@
 package linkedList;
+
 import node.Node;
+
 public class LinkedList {
 
     // CREATE CLASS VARIABLES, NODE CLASS, AND CONSTRUCTOR HERE //
@@ -13,8 +15,7 @@ public class LinkedList {
     private int length;
 
 
-
-    LinkedList(int value){
+    LinkedList(int value) {
         Node newNode = new Node(value);
         head = newNode;
         tail = newNode;
@@ -33,33 +34,33 @@ public class LinkedList {
         return length;
     }
 
-    public void append(int value){
+    public void append(int value) {
         Node newNode = new Node(value);
-       if (length == 0){
-           head = newNode;
-           tail = newNode;
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
 
-       } else {
-           tail.next = newNode;
-           tail = newNode;
-       }
-       length++;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        length++;
     }
 
-    public Node removeLast(){
-        if (length == 0){
+    public Node removeLast() {
+        if (length == 0) {
             return null;
         }
         Node temp = head;
         Node pre = head;
-        while (temp.next != null){
+        while (temp.next != null) {
             pre = temp;
             temp = temp.next;
         }
         tail = pre;
         tail.next = null;
         length--;
-        if (length == 0){
+        if (length == 0) {
             head = null;
             tail = null;
         }
@@ -67,9 +68,9 @@ public class LinkedList {
 
     }
 
-    public void prepend(int value){
+    public void prepend(int value) {
         Node newNode = new Node(value);
-        if (length == 0){
+        if (length == 0) {
             head = newNode;
             tail = newNode;
         } else {
@@ -80,31 +81,61 @@ public class LinkedList {
         length++;
     }
 
-    public Node removeFirst(){
-        if (length == 0){
+    public Node removeFirst() {
+        if (length == 0) {
             return null;
         }
         Node temp = head;
         head = head.next;
         temp.next = null; //this will remove the reference from the first item to the second item. If you don't do this, the first item will still be pointing to the second item, and the second item will still be pointing to the third item, and so on. This will cause a memory leak. instead this will result in garbage collection.
         length--;
-        if (length == 0){
+        if (length == 0) {
             tail = null;
         }
         return temp; //because of this return statement, the value of the removed node can be accessed in App.java. If you don't want to return the value, you can just return null.
     }
 
-    public Node get(int index){
-        if (index >= length || index < 0){
+    public Node get(int index) {
+        if (index >= length || index < 0) {
             return null;
         }
         Node temp = head;
-        for (int i = 0; i < index; i++){
+        for (int i = 0; i < index; i++) {
             temp = temp.next;
         }
         return temp;
     }
 
+    public boolean set(int index, int value) {
+        Node temp = get(index);
+        if (temp != null) {
+            temp.value = value;
+            return true;
+        }
+        return false;
+
+
+    }
+
+    public boolean insert(int index, int value) {
+        if (index < 0 || index > length) {
+            return false;
+        }
+        if (index == 0) {
+            prepend(value);
+            return true;
+        }
+        if (index == length) {
+            append(value);
+            return true;
+        }
+        Node newNode = new Node(value);
+        Node temp = get(index - 1);
+        newNode.next = temp.next;
+        temp.next = newNode;
+        length++;
+        return true;
+    }
     public void printList() {
         Node temp = head;
         while (temp != null) {
