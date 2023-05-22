1b314e962a2f9f814ba129b651cacc98d87ced88
shoaib <shoaibhossain24@gmail.com>
2023-05-23 00:47:31 +0600

    Add findMiddleNode

--- src/linkedList/LinkedList.java
+++ src/linkedList/LinkedList.java
@@ -202,5 +202,23 @@ public Node remove(int index) {
         }
     }
 
+
+    public Node findMiddleNode(){
+
+        if(head ==null){
+            return null;
+        }
+
+        Node slow,fast;
+        slow=head;
+        fast = head;
+
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
 }
 
