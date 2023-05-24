e30e8849eb4556532c1347fa799ecb6e4c552521
shoaib <shoaibhossain24@gmail.com>
2023-05-24 18:33:24 +0600

    Add findKthFromEnd method to find the Kth term form the end

--- src/linkedList/LinkedList.java
+++ src/linkedList/LinkedList.java
@@ -236,5 +236,30 @@ public Node remove(int index) {
         return false;
     }
 
+    public Node findKthFromEnd(int k){
+
+        Node fast = head;
+        Node slow = head;
+
+        for(int i=0; i<k; i++){
+
+            if(fast == null){
+                return null;
+            }
+            fast = fast.next;
+        }
+
+        while (fast !=null){
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+
+
+
+
+        return slow;
+    }
+
 }
 
