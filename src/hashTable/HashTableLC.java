8ebe0fe114a7e6d9c390bdf0f246840f9bfe4e9c
shoaib <shoaibhossain24@gmail.com>
2023-07-23 18:10:09 +0600

    Add hashTable LC findDuplicates method

--- src/hashTable/HashTableLC.java
+++ src/hashTable/HashTableLC.java
@@ -7,10 +7,20 @@ public class HashTableLC {
     public static void main(String[] args){
 
         //test for itemInCommon
-        int[] array1 = {1,2,3,4,5};
-        int[] array2 = {6,7,5,8,9,10};
+//        int[] array1 = {1,2,3,4,5};
+//        int[] array2 = {6,7,5,8,9,10};
+//
+//        System.out.println("Items In Common : "+itemInCommon(array1,array2));
 
-        System.out.println("Items In Common : "+itemInCommon(array1,array2));
+
+        //test for findDuplicates
+        ArrayList<Integer> array = new ArrayList<Integer>();
+        array.add(1);
+        array.add(2);
+        array.add(3);
+        array.add(2);
+
+        System.out.println("Duplicates : "+findDuplicates(array));
     }
 
 
@@ -34,5 +44,27 @@ public class HashTableLC {
         // if loop finishes, return false
 
         return false;
+    }
+
+
+    public static List<Integer> findDuplicates(ArrayList<Integer> array){
+
+        // create hash table
+        HashMap<Integer,Boolean> myHashMap = new HashMap<Integer,Boolean>();
+        // create list to return
+        List<Integer> duplicates = new ArrayList<Integer>();
+        // loop through array
+        for(int i: array){
+            // if item is in hash table, add to list
+            if(myHashMap.containsKey(i)){
+                duplicates.add(i);
+            }
+            // else add item to hash table
+            else{
+                myHashMap.put(i,true);
+            }
+        }
+        // return list
+        return duplicates;
     }
 }
