bc91b74474f754660e6bcbfd96934955d0369366
shoaib <shoaibhossain24@gmail.com>
2023-08-18 20:07:22 +0600

    Add heap insert

--- src/heap/App.java
+++ src/heap/App.java
@@ -4,5 +4,28 @@ public class App {
     public static void main(String[] args){
         System.out.println("This is the output for heap app.");
 
+        Heap myHeap = new Heap();
+
+        myHeap.insert(99);
+        myHeap.insert(80);
+        myHeap.insert(75);
+        myHeap.insert(60);
+        myHeap.insert(68);
+        myHeap.insert(55);
+
+
+        System.out.println(myHeap.getHeap());
+
+        System.out.println("After inserting 100 into the heap");
+        myHeap.insert(100);
+
+
+        System.out.println(myHeap.getHeap());
+
+
+        System.out.println("After inserting 75 into the heap");
+        myHeap.insert(75);
+
+        System.out.println(myHeap.getHeap());
     }
 }
