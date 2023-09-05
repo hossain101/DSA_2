9a94a7c4f9ccd76634ec5b98ccd9d397a9e6143f
shoaib <shoaibhossain24@gmail.com>
2023-09-05 17:24:32 +0600

    Add Recursion

--- src/recursion/App.java
+++ src/recursion/App.java
@@ -7,5 +7,15 @@ public class App {
     public static void main(String[] args){
         Recursion newRecursion = new Recursion();
         newRecursion.method1();
+
+
+       double result =  newRecursion.factorial(5);
+
+
+        System.out.println("Factorial result: "+ result);
+
+
     }
+
+
 }
--- src/recursion/Recursion.java
+++ src/recursion/Recursion.java
@@ -4,8 +4,6 @@ public class Recursion {
     public void method1(){
         method2();
         System.out.println("Printing method 1 yoo");
-
-
     }
 
     public void method2(){
@@ -16,5 +14,18 @@ public class Recursion {
     public void method3(){
 
         System.out.println("Printing method 3 yoo");
+    }
+
+
+    public double factorial(double n){
+        if(n==1) return 1;
+
+
+       double result = n* factorial(n-1); // The result is n * the result from the previous recursion.
+
+        System.out.println(result);
+
+        return result;
+
     }
 }
